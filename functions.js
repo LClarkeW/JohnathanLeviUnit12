76ce8a2fa0ef01ffb050b6df5283ad0b03936f5d
Levi C Williams <lw71724@srcs.loc>
2017-02-17 13:44:18 -0800

    Just Married

---
+++ functions.js
@@ -0,0 +1,96 @@
+/**
+ * Displays the area code for an inputted phone number
+ * @param {string} inputId  The element id for the text box
+ * @param {string} outputId The element id of message div
+ */
+function displayEverything(inputId, outputId,output1Id,output2Id) {
+    var outputText = "";
+    var outputTextCO = "";
+    var outputTextLine = "";
+    var phoneNum = document.getElementById(inputId).value;
+    // Now try to get the code
+    try {
+        var areaCode = getAreaCode(phoneNum.replace(/ /g, ""))
+        outputText = "Your area code is " + areaCode;
+    }
+    catch (error) {
+        console.log(error.message);
+        outputText = error.message;
+    document.getElementById(outputId).innerHTML = outputText;
+
+    try {
+        outputTextCO = "Your CO code is " + COC;
+    }
+    catch (error) {
+        console.log(error.message);
+        outputTextCO = error.message;
+    document.getElementById(output1Id).innerHTML = outputTextCO;
+
+    try {
+        outputTextLine = "Your Line code is " + line;
+    }
+    catch (error) {
+        console.log(error.message);
+        outputTextLine = error.message;
+    document.getElementById(output2Id).innerHTML = outputTextLine;
+    }
+}
+
+function getAreaCode(phoneNum) {
+    var areaCode;
+    try {
+        areaCode = between(phoneNum, "(", ")");
+        areaCode = areaCode.trim();
+        if (areaCode.length == 3 && Number(areaCode)) {
+            return areaCode;
+        }
+        else {
+            throw new Error("Invalid area code: " + areaCode);
+        }
+    }
+    catch (error) {
+        throw new Error("Invalid phone number: " + error.message);
+    }
+}
+
+function between(string, start, end) {
+    var startAt = string.indexOf(start);
+    startAt += start.length;
+    var endAt = string.indexOf(end, startAt);
+    return string.slice(startAt, endAt);
+}
+
+function getCOCode(phoneNum) {
+    var COC;
+    try {
+        COC = between(phoneNum, ")", "-");
+        COC = COC.trim();
+        if (COC.length == 3 && Number(COC)) {
+            return COC;
+        }
+        else {
+            throw new Error("Invalid CO code: " + COC);
+        }
+    }
+    catch (error) {
+        throw new Error("Invalid phone number: " + error.message);
+    }
+}
+
+function getLineCode(phoneNum) {
+    var line;
+    try {
+        line = phoneNum.slice(9,15);
+        line = line.trim();
+        if (line.length == 3 && Number(line)) {
+            return line;
+        }
+        else {
+            throw new Error("Invalid line code: " + line);
+        }
+    }
+    catch (error) {
+        throw new Error("Invalid phone number: " + error.message);
+    }
+}
+
